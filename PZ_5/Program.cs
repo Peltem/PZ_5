4ef2a506877fcfca691c4aead37385dfe8594885
student <student@B206-11>
2023-02-16 15:01:38 +0700

    Добавьте файлы проекта.

---
+++ PZ_5/Program.cs
@@ -0,0 +1,40 @@
+﻿static void Main(string[] args)
+{
+
+    Publisher publisher = new Publisher();
+    ConcreteSubscriber s = new ConcreteSubscriber();
+    publisher.Subscribe(s);
+
+}
+
+public class Publisher
+{
+
+    private Subscruber[] _subscribers;
+    private string _mainState;
+    public void Subscribe(Subscruber s)
+    {
+
+    }
+    public void UnSubscribe(Subscruber s)
+    {
+        
+    }
+
+    public void notifySubscribers()
+    {
+
+    }
+    public void mainBuisnessLogic()
+    {
+
+    }
+}
+public interface Subscruber
+{
+
+}
+public class ConcreteSubscriber : Subscruber
+{
+
+}
